285b9520a3f772daeae994d72b74c902749bf42d
Deep0Thinking <Deep0Thinking@outlook.com>
2024-01-19 23:23:03 -0800

    Implement dynamic visibility for sticky header based on scroll direction

--- assets/js/custom-styles.js
+++ assets/js/custom-styles.js
@@ -3,23 +3,21 @@
  */
 
 // Toggle Display (none <-> block)
+
 var x = document.getElementsByClassName("Text");
 var y = document.getElementsByClassName("Selection");
 var z = document.getElementsByClassName("Single-Container");
 
-for (var i = 0, len = x.length; i < len; i++)
-{
-    (function(index){
-        z[i].onclick = function(){
+for (var i = 0, len = x.length; i < len; i++) {
+    (function (index) {
+        z[i].onclick = function () {
 
-            if (x[index].style.display === "none")
-            {
+            if (x[index].style.display === "none") {
                 x[index].style.display = "flex";
                 y[index].style.display = "none";
             }
 
-            else
-            {
+            else {
                 x[index].style.display = "none";
                 y[index].style.display = "flex";
             }
@@ -29,3 +27,24 @@ for (var i = 0, len = x.length; i < len; i++)
     })(i);
 
 }
+
+/**
+ * default.md
+ */
+
+// Dynamic visibility for sticky header based on scroll direction
+
+let lastScrollTop = 0; // This variable will hold the last scroll position
+
+window.addEventListener("scroll", function () {
+    let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (currentScroll > lastScrollTop) {
+        // Scrolling down
+        document.querySelector('.site-header').classList.add('hide-site-header');
+    } else {
+        // Scrolling up
+        document.querySelector('.site-header').classList.remove('hide-site-header');
+    }
+    lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // For Mobile or negative scrolling
+}, false);
